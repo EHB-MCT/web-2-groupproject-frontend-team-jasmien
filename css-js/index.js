7e30d8a413ad2102b478516024200db9b9aeb173
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-12-05 16:12:37 +0100

    js show challenges

--- css-js/index.js
+++ css-js/index.js
@@ -1,15 +1,33 @@
-async function loadingHeroku(){
+async function loadingHeroku() {
     let url = `https://team-jasmien.herokuapp.com/allChallenges`;
     let resp = await fetch(url);
     return await resp.json()
 }
 
-window.onload = function(){
+window.onload = function () {
     console.log('Loaded!');
 
-    async function run(){
+    async function run() {
         let data = await loadingHeroku()
         console.log(data)
+
+        data.forEach(element => {
+            let htmlString = `
+            <div id="containerItem">
+            <h2>Id: ${element._id}</h2>
+            <h2>Name: ${element.name}</h2>
+            <h2>Points: ${element.points}</h2>
+            <h2>Course: ${element.course}</h2>
+            <h2>Session: ${element.sessions}</h2>
+            <hr id="hr">
+            </div>`;
+            
+            console.log(htmlString)
+
+            let container = document.getElementById('containerList');
+            container.insertAdjacentHTML('beforeend', htmlString);
+        });
+        
     }
 
     run();
