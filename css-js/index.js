20fe8fd012d4f9ab414833f757a06a0bb83af0ef
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-07 10:59:20 +0100

    Oplossing dataGet is not defined + 400 blijft

--- css-js/index.js
+++ css-js/index.js
@@ -1,17 +1,43 @@
 window.onload = function () {
     console.log('Loaded!');
-    loadingHeroku()
+    run();
     document.getElementById('form').addEventListener('submit', e => {
         e.preventDefault();
         form();
     })
 }
 
+async function loadingHeroku() {
+    let url = `https://team-jasmien.herokuapp.com/allChallenges`;
+    let resp = await fetch(url);
+    return await resp.json()
+}
+
+async function run() {
+    let dataGet = await loadingHeroku()
+    console.log(dataGet)
+    dataGet.forEach(element => {
+        let container = document.getElementById('listBlock');
+        let htmlString = `
+            <h1 id="titleListBlock">Challenge</h1>
+            <div id="containerList">
+                <h2>Name: ${element.name}</h2>
+                <h2>Points: ${element.points}</h2>
+                <h2>Course: ${element.course}</h2>
+                <h2>Session: ${element.sessions}</h2>
+                <hr id="hr">            
+            </div>`;
+        console.log(htmlString)
+        container.insertAdjacentHTML('beforeend', htmlString);  
+    })
+      
+}
+
 function form(){
-    const name = document.getElementById('challenge').value;
-    const points = document.getElementById('points').value;
-    const course = document.getElementById('course').value;
-    const session = document.getElementById('session').value
+    let name = document.getElementById('challenge').value;
+    let points = document.getElementById('points').value;
+    let course = document.getElementById('course').value;
+    let session = document.getElementById('session').value
         
     console.log(name, points, course, session);
 
@@ -28,28 +54,30 @@ function form(){
     .then(dataPost=> {
         console.log("Succes Post", dataPost)
     })
-    loadingHeroku()       
+    run()       
 }
 
-async function loadingHeroku() {
+// function loadingHeroku() {
 
-    await fetch('https://team-jasmien.herokuapp.com/allChallenges')
-    .then(response => response.json())
-    .then(dataGet => 
-        console.log("SuccesGET", dataGet))
-        dataGet.forEach(element => {
-            let container = document.getElementById('listBlock');
-            let htmlString = `
-                <h1 id="titleListBlock">Challenge</h1>
-                <div id="containerList">
-                    <h2>Name: ${element.name}</h2>
-                    <h2>Points: ${element.points}</h2>
-                    <h2>Course: ${element.course}</h2>
-                    <h2>Session: ${element.sessions}</h2>
-                    <hr id="hr">
-                </div>`;
+//     fetch('https://team-jasmien.herokuapp.com/allChallenges')
+//     .then(response => response.json())
+//     .then(dataGet => 
+//         console.log("SuccesGET", dataGet))
+//         let data = dataGet
+//         console.log("Test", data);
+//         data.forEach(element => {
+//             let container = document.getElementById('listBlock');
+//             let htmlString = `
+//                 <h1 id="titleListBlock">Challenge</h1>
+//                 <div id="containerList">
+//                     <h2>Name: ${element.name}</h2>
+//                     <h2>Points: ${element.points}</h2>
+//                     <h2>Course: ${element.course}</h2>
+//                     <h2>Session: ${element.sessions}</h2>
+//                     <hr id="hr">
+//                 </div>`;
             
-            console.log(htmlString)
-            container.insertAdjacentHTML('beforeend', htmlString);
-        });
-}
+//             console.log(htmlString)
+//             container.insertAdjacentHTML('beforeend', htmlString);
+//         });
+// }
