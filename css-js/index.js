1984061cfaede6514b198aa8e0feaeeb342525c2
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-07 10:13:56 +0100

    Bad Request -> 400

--- css-js/index.js
+++ css-js/index.js
@@ -1,3 +1,25 @@
+let submitForm = document.getElementById("form");
+submitForm.addEventListener("submit", e => {
+    e.preventDefault();
+  
+    let inputData = ({
+      "name": document.getElementById("challenge").value,
+      "points": document.getElementById("points").value,
+      "course": document.getElementById("course").value,
+      "session": document.getElementById("session").value
+    });
+  
+    fetch("https://team-jasmien.herokuapp.com/saveChallenge", {
+        method: 'POST',
+        body: inputData
+    })
+      .then(response => response.json())
+      .then(data2Post=> console.log(dataPost))
+      .catch(error => console.log('error', error));
+})
+  
+
+
 async function loadingHeroku() {
     let url = `https://team-jasmien.herokuapp.com/allChallenges`;
     let resp = await fetch(url);
@@ -8,19 +30,18 @@ window.onload = function () {
     console.log('Loaded!');
 
     async function run() {
-        let data = await loadingHeroku()
-        console.log(data)
+        let dataGet = await loadingHeroku()
+        console.log(dataGet)
 
-        data.forEach(element => {
+        dataGet.forEach(element => {
             let htmlString = `
-            <div id="containerItem">
-            <h2>Id: ${element._id}</h2>
-            <h2>Name: ${element.name}</h2>
-            <h2>Points: ${element.points}</h2>
-            <h2>Course: ${element.course}</h2>
-            <h2>Session: ${element.sessions}</h2>
-            <hr id="hr">
-            </div>`;
+                <div id="containerItem">
+                    <h2>Name: ${element.name}</h2>
+                    <h2>Points: ${element.points}</h2>
+                    <h2>Course: ${element.course}</h2>
+                    <h2>Session: ${element.sessions}</h2>
+                    <hr id="hr">
+                </div>`;
             
             console.log(htmlString)
 
