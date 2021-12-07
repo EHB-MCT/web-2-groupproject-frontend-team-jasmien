deda464b4194c488f5844aa2a62a3a120bf1b97e
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-12-07 13:52:45 +0100

    verder in 403 -> input niet meegestuurd

--- css-js/index.js
+++ css-js/index.js
@@ -1,6 +1,5 @@
 window.onload = function () {
     console.log('Loaded!');
-    run();
     document.getElementById('form').addEventListener('submit', e => {
         e.preventDefault();
         form();
@@ -17,6 +16,7 @@ async function run() {
     let dataGet = await loadingHeroku()
     console.log(dataGet)
     dataGet.forEach(element => {
+        console.log(element);
         let container = document.getElementById('listBlock');
         let htmlString = `
             <h1 id="titleListBlock">Challenge</h1>
@@ -34,48 +34,21 @@ async function run() {
 }
 
 function form(){
-    let name = document.getElementById('challenge').value;
-    let points = document.getElementById('points').value;
-    let course = document.getElementById('course').value;
-    let session = document.getElementById('session').value
-        
-    let object = name, points, course, session;
-    console.log(name, points, course, session);
+    let object = {
+        'name': document.getElementById('challenge').value,
+        'points': document.getElementById('points').value,
+        'course': document.getElementById('course').value,
+        'session': document.getElementById('session').value
+    }
+    console.log(object.name);
 
     fetch("https://team-jasmien.herokuapp.com/saveChallenge", {
         method: 'POST',
-        body: JSON.stringify({
-            object
-        }),
+        body: JSON.stringify({object}),
     })    
     .then(response => response.json())
     .then(dataPost=> {
         console.log("Succes Post", dataPost)
     })
-    loadingHeroku()       
+    run()       
 }
-
-// function loadingHeroku() {
-
-//     fetch('https://team-jasmien.herokuapp.com/allChallenges')
-//     .then(response => response.json())
-//     .then(dataGet => 
-//         console.log("SuccesGET", dataGet))
-//         let data = dataGet
-//         console.log("Test", data);
-//         data.forEach(element => {
-//             let container = document.getElementById('listBlock');
-//             let htmlString = `
-//                 <h1 id="titleListBlock">Challenge</h1>
-//                 <div id="containerList">
-//                     <h2>Name: ${element.name}</h2>
-//                     <h2>Points: ${element.points}</h2>
-//                     <h2>Course: ${element.course}</h2>
-//                     <h2>Session: ${element.sessions}</h2>
-//                     <hr id="hr">
-//                 </div>`;
-            
-//             console.log(htmlString)
-//             container.insertAdjacentHTML('beforeend', htmlString);
-//         });
-// }
